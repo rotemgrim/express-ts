12867e52b352fb5fe808c8b940a7b3353db384ee
rotem <rotemgrim@gmail.com>
2019-06-20 18:47:38 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-import HaploApp from "./app/App";
+import App from "./app/App";
 import { Container } from "typedi";
-
 import { exampleMiddleware } from "./middlewares/ExampleMiddleware";
 import { baseRoutes } from "./routes/baseRoutes";
+import { config } from "./config/config";
+import DBConnection from "./config/dbConnection";
 
-const haplo: HaploApp = Container.get(HaploApp);
+Container.set("config", config);
 
-haplo.setGlobalMiddlewares([
+DBConnection.connect(config.)
+
+const server: App = Container.get(App);
+
+server.setGlobalMiddlewares([
     exampleMiddleware,
 ]);
 
-haplo.setRoutes([
+server.setRoutes([
     baseRoutes,
 ]);
 
-const app = haplo.start();
+const app = server.start();
 
 export default app;
--- src/routes/baseRoutes.ts
+++ src/routes/baseRoutes.ts
@@ -1,12 +1,12 @@
 import { IRouter } from "express";
-import { RoutesBatch, App } from "../haplo/App";
+import { RoutesBatch, App } from "../app/App";
 import ExampleController from "../controllers/ExampleController";
 import { exampleMiddleware } from "../middlewares/ExampleMiddleware";
 
 
 export const baseRoutes: RoutesBatch = (app: App) => {
-    
-    app.get('/', ExampleController.index);
-    app.get('/2', ExampleController.index);
 
-} 
+    app.get("/", ExampleController.index);
+    app.get("/2", ExampleController.index);
+
+};
